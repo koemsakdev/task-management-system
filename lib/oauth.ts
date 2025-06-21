96ef5e48c7ed7c8275fc89295987215340729ef6
Koemsak <meankoemsak@gmail.com>
2025-06-21 16:58:10 +0700

    Fix oauth in PROD

--- lib/oauth.ts
+++ lib/oauth.ts
@@ -2,13 +2,12 @@
 
 import { createAdminClient } from "@/lib/appwrite";
 import { redirect } from "next/navigation";
-import { headers } from "next/headers";
 import { OAuthProvider } from "node-appwrite";
 
 export async function signUpWithGithub() {
 	const { account } = await createAdminClient();
 
-  const origin = (await headers()).get("origin");
+  const origin = process.env.NEXT_PUBLIC_AUTH_URL;
   
 	const redirectUrl = await account.createOAuth2Token(
 		OAuthProvider.Github,
@@ -22,7 +21,7 @@ export async function signUpWithGithub() {
 export async function signUpWithGoogle() {
 	const { account } = await createAdminClient();
 
-  const origin = (await headers()).get("origin");
+  const origin = process.env.NEXT_PUBLIC_AUTH_URL;
   
 	const redirectUrl = await account.createOAuth2Token(
 		OAuthProvider.Google,
